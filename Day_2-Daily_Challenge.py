009fb904008f209b84fde78afcec909c86056865
Missael <jonathanmissael_jimenez@ucol.mx>
2020-08-10 10:45:51 -0500

    Respuesta correcta y explicacion agregada

--- Day_2-Daily_Challenge.py
+++ Day_2-Daily_Challenge.py
@@ -7,10 +7,11 @@ squares = [1, 4, 9, 16, 25]
 # My_Solution
 # What is the output of this code?
 # My_Answer=9
-# Checks
-print(squares[-3:])
 
-# Explain
+# Checks
+# print(squares[-3:])
+
+# Explanation
 # Python knows a number of compound data types, used to group together other values. The most versatile is the list, which can be written as a list of comma-separated values (items) between square brackets. Lists might contain items of different types, but usually the items all have the same type. Like strings (and all other built-in sequence type), lists can be indexed and sliced.
 
 # Challenge_2
@@ -29,3 +30,11 @@ print(s)
 # My_Answer=[1,2,3,4]
 
 # Checks
+# Answer=0
+# Explanation
+# In the puzzle we define our own range function srange(x).
+# It creates a range object using the built-in range() function.
+# Calling range(start, stop) returns a range object with range-values from start to stop-1.
+# Therefore srange(x) returns a range from x-3 to x-1.
+# When we call the function srange() with x=2 it returns a range object with the values -1, 0, 1.
+# In the loop we sum up these values and get the output 0.
