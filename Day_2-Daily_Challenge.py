6a8f2bb34a42fd740a696cb5d229551adca63f0d
Missael <jonathanmissael_jimenez@ucol.mx>
2020-08-10 11:42:36 -0500

    Secuencia

--- Day_2-Daily_Challenge.py
+++ Day_2-Daily_Challenge.py
@@ -70,10 +70,11 @@ print(x)
 
 # Challenge_4
 # SequencePuzzle
-[30, 9, 40, 50, 91, 142, 234]
+seq = [30, 9, 40, 50, 91, 142, 234]
 
 print('Challenge_4')
 
+
+seq[i] = seq[i-2] + seq[i-1] + 1
 # What's the next element in the list?
 # My_Answer=377
-seq[i] = seq[i-2] + seq[i-1] + 1
