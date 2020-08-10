0ed1a9564235043b5237068e348723078a11bb15
Missael <jonathanmissael_jimenez@ucol.mx>
2020-08-10 10:25:27 -0500

    Mi Respuesta es correcta.

--- Day_2-Daily_Challenge.py
+++ Day_2-Daily_Challenge.py
@@ -1,5 +1,9 @@
 squares = [1, 4, 9, 16, 25]
-print(squares[-3])
+# print(squares[-3])
 # What is the output of this code?
 
 # My_Solution
+# What is the output of this code?
+# Answer=9
+# Checks
+print(squares[-3])
