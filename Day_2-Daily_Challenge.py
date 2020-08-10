b95a150adfd450d5f59c34a5e1475fe7b31cdce5
Missael <jonathanmissael_jimenez@ucol.mx>
2020-08-10 10:32:48 -0500

    Agregando Reto 2

--- Day_2-Daily_Challenge.py
+++ Day_2-Daily_Challenge.py
@@ -1,3 +1,5 @@
+# Challenge_1
+
 squares = [1, 4, 9, 16, 25]
 # print(squares[-3]:)
 # What is the output of this code?
@@ -10,3 +12,15 @@ print(squares[-3:])
 
 # Explain
 # Python knows a number of compound data types, used to group together other values. The most versatile is the list, which can be written as a list of comma-separated values (items) between square brackets. Lists might contain items of different types, but usually the items all have the same type. Like strings (and all other built-in sequence type), lists can be indexed and sliced.
+
+# Challenge_2
+
+
+def srange(x):
+    return range(x-3, x)
+
+
+s = 0
+for i in srange(2):
+    s += i
+print(s)
