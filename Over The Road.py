7f4e7e584f1370e2a1a830f4c9238d77cd4a38db
Missael <jonathanmissael_jimenez@ucol.mx>
2020-08-10 00:35:46 -0500

    Cambio en Primera Seccion para reducir el procesamiento

--- Over The Road.py
+++ Over The Road.py
@@ -12,7 +12,14 @@ def over_the_road(address, n):
     address = 1
     total_house = n*2
     if n == 3:
-        print('1|', '|6\n3|', '|4\n5|', '|2', sep='\t')
+        if address % 2 == 0:
+                print(str(total_house)+'|', '|'+str(address)+'\n', sep='\t')
+                total_house = total_house-1
+                address = address+1
+            else:
+                print(str(address)+'|', '|'+str(total_house)+'\n', sep='\t')
+                total_house = total_house-1
+                address = address+1
     else:
         while n < total_house:
             if address % 2 == 0:
