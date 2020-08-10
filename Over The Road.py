33ce86c943089b2788e88a1a2e792e4fda165cc8
Missael <jonathanmissael_jimenez@ucol.mx>
2020-08-10 00:38:54 -0500

    Agregando el mismo While

--- Over The Road.py
+++ Over The Road.py
@@ -12,14 +12,15 @@ def over_the_road(address, n):
     address = 1
     total_house = n*2
     if n == 3:
-        if address % 2 == 0:
-            print(str(total_house)+'|', '|'+str(address)+'\n', sep='\t')
-            total_house = total_house-1
-            address = address+1
-        else:
-            print(str(address)+'|', '|'+str(total_house)+'\n', sep='\t')
-            total_house = total_house-1
-            address = address+1
+        while n < total_house:
+            if address % 2 == 0:
+                print(str(total_house)+'|', '|'+str(address)+'\n', sep='\t')
+                total_house = total_house-1
+                address = address+1
+            else:
+                print(str(address)+'|', '|'+str(total_house)+'\n', sep='\t')
+                total_house = total_house-1
+                address = address+1
     else:
         while n < total_house:
             if address % 2 == 0:
